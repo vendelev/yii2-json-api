96415fce1d29bb52c7ba2345cd18b560c361f223
Anton Tuyakhov <atuyakhov@gmail.com>
2016-03-16 21:35:25 +0200

    initial commit

---
+++ src/JsonApiParser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Anton Tuyakhov <atuyakhov@gmail.com>
+ */
+
+namespace tuyakhov\jsonapi;
+
+use \yii\web\JsonParser;
+
+class JsonApiParser extends JsonParser
+{
+    public function parse($rawBody, $contentType)
+    {
+        // TODO probably need to validate 'type' property.
+        return parent::parse($rawBody, $contentType);
+    }
+
+}
