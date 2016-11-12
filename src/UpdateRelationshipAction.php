736a62078a767aae97f96d08562c4b281fd1a1e2
Anton Tuyakhov <atuyakhov@gmail.com>
2016-11-12 15:20:51 +0200

    add update relationships action

--- src/UpdateRelationshipAction.php
+++ src/UpdateRelationshipAction.php
@@ -7,19 +7,43 @@
 
 namespace tuyakhov\jsonapi;
 
-
-use yii\helpers\Inflector;
+use yii\db\BaseActiveRecord;
 use yii\rest\Action;
+use yii\web\BadRequestHttpException;
 use yii\web\ServerErrorHttpException;
 
 class UpdateRelationshipAction extends Action
 {
+    /**
+     * @param $id
+     * @param $name
+     * @throws BadRequestHttpException
+     * @throws ServerErrorHttpException
+     */
     public function run($id, $name)
     {
+        /** @var BaseActiveRecord $model */
         $model = $this->findModel($id);
 
-        if ($model instanceof ResourceInterface) {
-            \Yii::$app->getRequest()->getBodyParams();
+        if (!$model instanceof ResourceInterface) {
+            throw new BadRequestHttpException('Impossible to update relationships for resource');
+        }
+
+        $data = \Yii::$app->getRequest()->getBodyParams();
+        $relationships = [];
+        foreach ($data as $modelName => $identifier) {
+            if (!isset($identifier['id'])) {
+                continue;
+            }
+            /** @var BaseActiveRecord $modelName */
+            if ($relationship = $modelName::findOne($identifier['id'])) {
+                $relationships[] = $relationship;
+            }
+        }
+
+        if (!empty($relationships)) {
+            $model->unlinkAll($name);
+            $model->setResourceRelationship($name, $relationships);
         }
 
         throw new ServerErrorHttpException('Failed to update the relationship for unknown reason.');
